172c592e90ae51764d1e4098fc1c90d15eeb82e0
Scott <scott.penneman@gmail.com>
2020-02-15 23:06:32 -0600

    Assignment #3 without bonus missions

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -1,7 +1,6 @@
 package org.launchcode.javawebdevtechjobsmvc.controllers;
 
-import org.launchcode.javawebdevtechjobsmvc.models.Job;
-import org.launchcode.javawebdevtechjobsmvc.models.JobData;
+import org.launchcode.javawebdevtechjobsmvc.models.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -24,5 +23,32 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping("results")
+    public String displaySearchResults (Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+//        ArrayList<Job> jobs;
+//        if (searchType.toLowerCase().equals("all")){
+//            jobs = JobData.findAll();
+//            model.addAttribute("title", "All Jobs");
+//        } else {
+//            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+//            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+//        }
+//        model.addAttribute("jobs", jobs);
+
+        ArrayList<Job> jobs;
+        if(searchTerm.toLowerCase().equals("all") || searchTerm.equals("")) {
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs by " + searchType + " including " + searchTerm);
+        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+
+    }
 
 }
